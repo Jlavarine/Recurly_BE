1638254d36a58e6b81bc24468ad1d407072916b1
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-07-15 17:46:52 -0500

    Add cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+
+app.use(cors())
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Recurly Assignment';
 
