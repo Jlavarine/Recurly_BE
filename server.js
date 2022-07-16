f124fc80c662933c23a230c2c14b768cd163d755
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-07-16 17:35:59 -0500

    Add comments for clarity

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 
 
 app.use(cors())
+// error handling for CORS errors
 app.use(express.json());
 app.set('port', process.env.PORT || 3001);
-app.locals.title = 'Recurly Assignment';
+app.locals.title = 'Recurly Take Home';
 
-app.get('/', (request, response) => {
+app.get('/', (response) => {
   response.send('Hello World!');
 });
 
@@ -19,6 +20,7 @@ app.listen(app.get('port'), () => {
 
 app.locals.account = []
 app.locals.updatedAddress = []
+// for storing the account and updatedAddress
 
 var request = require('request');
 var options = {
@@ -30,32 +32,25 @@ var options = {
     'Authorization': 'Basic OTBhZmRjYTkxMWZmNDJlNzlkN2ExMzRkY2YzOTRjMTc6'
   }
 };
+
 request(options, function (error, response) {
   if (error) throw new Error(error);
   app.locals.account = JSON.parse(response.body)
-
 });
+// request for GET to API 
 
-app.get('/api/v1/account', (request, response) => {
+app.get('/api/v1/account', (response) => {
     const account = app.locals.account;
     response.json({ account });
-  });
-
-
-// app.put('/api/v1/account', (request, response) => {
-//   console.log('request.body', request.body)
-//   app.locals.updatedAddress = response.body
-
-//   response.status(201).json('success');
-// });
+});
+// response for GET
 
 app.put('/api/v1/account', (request, response) => {
   app.locals.updatedAddress = request.body;
-
   app.locals.account.address = app.locals.updatedAddress
-
   response.status(201).json(app.locals.account);
 });
+// Response for PUT (returning updated account)
 
 var request = require('request');
 var options = {
@@ -73,3 +68,4 @@ request(options, function (error, response) {
   if (error) throw new Error(error);
   app.locals.account.address = JSON.parse(response.body)
 });
+// Request for PUT
